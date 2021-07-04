2791284bdf039934477620b2d156204336809524
Gyeonghun Kim <gyeonghun.kim.phys@gmail.com>
2021-07-04 10:15:19 +0900

    test reverse jordan wigner mapper based on jordan wigner mapper

--- test/mappers/second_quantization/test_reverse_jordan_wigner_mapper.py
+++ test/mappers/second_quantization/test_reverse_jordan_wigner_mapper.py
@@ -13,6 +13,7 @@
 """ Test Reverse Jordan Wigner Mapper """
 
 import unittest
+import math
 
 from test import QiskitNatureTestCase
 
@@ -20,16 +21,33 @@ from qiskit.opflow import X, Y, Z, I
 
 from qiskit_nature.drivers.second_quantization import HDF5Driver
 from qiskit_nature.mappers.second_quantization import JordanWignerMapper
+from qiskit_nature.mappers.second_quantization import ReverseJordanWignerMapper
 from qiskit_nature.properties.second_quantization.electronic import ElectronicEnergy
-
+from qiskit_nature.operators.second_quantization import FermionicOp
 
 #
 class TestReverseJordanWignerMapper(QiskitNatureTestCase):
     """Test Reverse Jordan Wigner Mapper"""
 
-    def test_mapping(self):
-        """Test mapping to qubit operator"""
-        raise NotImplementedError()
+    def test_mapping_based_on_jordan_wigner_mapper(self):
+        """Test mapping to one qubit operator"""
+        driver = HDF5Driver(
+            hdf5_input=self.get_resource_path(
+                "test_driver_hdf5.hdf5", "drivers/second_quantization/hdf5d"
+            )
+        )
+        q_molecule = driver.run()
+        fermionic_op = ElectronicEnergy.from_driver_result(q_molecule).second_q_ops()[0]
+        mapper = JordanWignerMapper()
+        qubit_op = mapper.map(fermionic_op)
+        reverse_mapper = ReverseJordanWignerMapper()
+        transformed_fermionic_op = reverse_mapper.map(qubit_op)
+
+        # Assert equal fails because of small difference with 1e-17 order. 
+        # self.assertEqual(fermionic_op, transformed_fermionic_op)
+
+        # check RMS difference is almost equal to zero
+        self.assertAlmostEqual(math.sqrt(sum([abs(x[1])**2 for x in (fermionic_op - transformed_fermionic_op).reduce().to_list()])), 0)
         
 if __name__ == "__main__":
     unittest.main()
