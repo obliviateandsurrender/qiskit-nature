3e55ecd53c84a3aa8605bb046810417bc71036dd
Utkarsh <utkarshazad98@gmail.com>
2021-07-06 18:13:39 +0530

    update map method for ReverseJordanWigner class

--- qiskit_nature/mappers/second_quantization/reverse_jordan_wigner_mapper.py
+++ qiskit_nature/mappers/second_quantization/reverse_jordan_wigner_mapper.py
@@ -12,64 +12,71 @@
 
 """Fermionic Mapper."""
 
-from abc import abstractmethod
-from itertools import product
-import math
-from typing import List, Tuple
-
 from qiskit.opflow import OperatorBase
 from qiskit_nature.operators.second_quantization import FermionicOp
-from qiskit.quantum_info import Pauli
+from qiskit.opflow.primitive_ops import PauliSumOp
 
 class ReverseJordanWignerMapper:
     """Reverse Jordan-Wigner mapping."""
 
-    def map(self, second_q_op: OperatorBase) -> FermionicOp:
-        """Maps a class:`OperatorBase` to a `FermionicOp`.
+    def map(self, qubit_op: PauliSumOp) -> FermionicOp:
+        
+        r""" Maps a `PauliSumOp` to a `FermionicOp` using the Jordan-Wigner transform.
+
+            Operators are mapped as follows:
+                Z_j -> I - 2 a^\dagger_j a_j -> I - 2 N_j
+                X_j -> (a^\dagger_j + a_j) Z_{j-1} Z_{j-2} .. Z_0
+                Y_j -> i (a^\dagger_j - a_j) Z_{j-1} Z_{j-2} .. Z_0
 
         Args:
-            second_q_op: the :class:`OperatorBase` to be mapped.
+            qubit_op: the :class:`PauliSumOp` to be mapped.
 
         Returns:
             The `FermionicOp` corresponding to the Hamiltonian in the Fermionic space.
         """
-
-        num_qubits = second_q_op.num_qubits # get number of qubits from input second quantized operator
-        fermionic_op = None
-        for term in second_q_op: 
-            transform_list : List[Tuple(str, float)] = []  # list of tuple(pauli, coeff)
-            pauli_op = term.to_pauli_op()
-            coef_term = pauli_op.coeff # coefficient for each pauli string term in input second quantized operator
-            target_pauli_op = pauli_op.primitive
-            for i in range(num_qubits):
-                one_pauli = target_pauli_op[num_qubits - 1 - i]
-                if one_pauli.to_label() == 'Z': # dealing Pauli Z op
-                    transform_list.append((('I', 1), ('N', -2))) # Zj -> I - 2*Nj => [('I', 1), ('N', -2)]
-                elif one_pauli.to_label() == 'X': # dealing Pauli X op
-                    transform_list.append((('+', 1), ('-', 1))) # Xj -> aj_dag + aj => [('+', 1), ('-', 1)]
-                    target_pauli_op &= Pauli("I" * (i+1) + "Z" * (num_qubits - i - 1)) # apply Z(j-1)Z(j-2) ... Z(0)
-                elif one_pauli.to_label() == 'Y': # dealing Pauli Y op
-                    transform_list.append((('+', -1j), ('-', 1j))) # Yj -> i(aj - aj_dag) => [('+', -1j), ('-', 1j)]
-                    target_pauli_op &= Pauli("I" * (i+1) + "Z" * (num_qubits - i - 1)) # apply Z(j-1)Z(j-2) ... Z(0)
-                else: # dealing Pauli I op
-                    transform_list.append((('I', 0.5), ('I', 0.5))) # Ij -> Ij => [('I', 0.5), ('I', 0.5)]; split I into 0.5I + 0.5I for code consistency
-            # dealing the phase
-            if target_pauli_op.phase == 1:
-                coef_term *= -1j
-            elif target_pauli_op.phase == 2:
-                coef_term *= -1
-            elif target_pauli_op.phase == 3:
-                coef_term *= 1j
+        
+        num_qubits = qubit_op.num_qubits # get number of qubits
+        fermionic_op = None 
+        fermionic_transforms = []
+        
+        for qb_op in qubit_op.to_pauli_op():
+            qb_fermionic_transforms = [] # store ferm_op for individual term
+            
+            # reverse traversing in accordance with qubit numbering
+            for qb_idx, op in enumerate(reversed(qb_op.primitive.to_label())):
                 
-            pauli_coefs = []
-            pauli_strings = []
-            # create fermionic operator for a term based on transform_list
-            for idxes in product(*[[0, 1]]*num_qubits):
-                pauli_coefs.append(math.prod([t[i][1] for t, i in zip(transform_list, idxes)]))
-                pauli_strings.append("".join([t[i][0] for t, i in zip(transform_list, idxes)])[::-1])
-            if not fermionic_op:
-                fermionic_op = coef_term * FermionicOp(list(zip(pauli_strings, pauli_coefs))).reduce()
-            else:
-                fermionic_op += coef_term * FermionicOp(list(zip(pauli_strings, pauli_coefs))).reduce()
-
-        return fermionic_op.reduce()
+                fermionic_transform = None # store transform for individual pauli terms
+                
+                # handle Pauli I (for consistency)
+                if op == 'I':
+                    fermionic_transform = FermionicOp(f"I_{qb_idx}", num_qubits)
+                # handle Pauli Z
+                elif op == 'Z':
+                    fermionic_transform = FermionicOp(f"I_{qb_idx}", num_qubits) - \
+                                            2*FermionicOp(f"N_{qb_idx}", num_qubits)
+                # handle Pauli X and Pauli Y
+                else:
+                    if op == 'X':
+                        fermionic_transform = FermionicOp(f"+_{qb_idx}", num_qubits) + \
+                                                FermionicOp(f"-_{qb_idx}", num_qubits)
+                    elif op == 'Y': 
+                        fermionic_transform = 1j*(FermionicOp(f"+_{qb_idx}", num_qubits) - \
+                                                FermionicOp(f"-_{qb_idx}", num_qubits))
+                    # handle exchange phase factor
+                    for ph_qb_idx in range(qb_idx, 0, -1):
+                        phase_term = FermionicOp(f"I_{ph_qb_idx-1}", num_qubits) - \
+                                        2*FermionicOp(f"N_{ph_qb_idx-1}", num_qubits)
+                        fermionic_transform = fermionic_transform.compose(phase_term)
+                    
+                qb_fermionic_transforms.append(fermionic_transform)
+            
+            transformed_term = qb_fermionic_transforms[0] * qb_op.coeff # coefficient of qubit_op term
+            for qb_term in qb_fermionic_transforms[1:]: # tensor product for individual term
+                transformed_term = transformed_term.compose(qb_term)
+            fermionic_transforms.append(transformed_term)
+    
+        fermionic_op = fermionic_transforms[0] 
+        for fermion_term in fermionic_transforms[1:]: # sum up individual fermionic terms
+            fermionic_op = fermionic_op.add(fermion_term)
+        
+        return fermionic_op.reduce() # for merging terms with same labels
